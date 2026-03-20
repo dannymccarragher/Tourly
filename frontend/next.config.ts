e59900e4ed57337bd53c0e5d736e0fed6353af00
Danny McCarragher <dannymccarragher@gmail.com>
2026-03-20 13:35:21 -0700

    include proxy in config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     "127.0.0.1",
     '10.0.0.130'
   ],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://chartify-786g.onrender.com/:path*'
+      }
+    ];
+  }
 };
 
 export default nextConfig;
